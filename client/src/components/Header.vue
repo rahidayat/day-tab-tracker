d22596918bc2841d1bb0f95cea4630720354859a
rahid <hidayat.85@gmail.com>
2017-09-17 22:21:06 +0700

    create component Header and routing register component in it

---
+++ client/src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-toolbar fixed class="teal" dark>
+      <v-toolbar-title class="mr-4">
+        <!-- <router-link to="/"> -->
+          <span class="home" @click="navigateTo({name: 'root'})">
+            Day-TabTracker
+          </span>
+        <!-- </router-link> -->
+      </v-toolbar-title>
+
+    <v-toolbar-items>
+      <!-- <router-link to=""> -->
+        <v-btn flat dark>
+          Browse
+        </v-btn>
+      <!-- </router-link> -->
+    </v-toolbar-items>
+
+    <v-spacer/>
+
+    <v-toolbar-items>
+      <!-- <router-link to="register"> -->
+        <v-btn flat dark @click="navigateTo({name: 'register'})">
+          Sign Up
+        </v-btn>
+      <!-- </router-link> -->
+    </v-toolbar-items>
+
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .home {
+    cursor: pointer;
+  }
+
+  .home:hover {
+    color: #e78e
+  }
+
+</style>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -7,13 +7,12 @@
         </v-toolbar>
 
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <input type="email" name="email" placeholder="email" v-model="email">
-          <br>
-          <input type="password" name="password" placeholder="password" v-model="password">
-          <br>
+          <v-text-field label="E-mail" v-model="email"></v-text-field>
+          <v-text-field type="password" label="Password" v-model="password"></v-text-field>
+
           <div class="error" v-html="error"></div>
           <br>
-          <v-btn class="cyan" @click="register">Register</v-btn>
+          <v-btn class="cyan" dark @click="register">Register</v-btn>
         </div>
       </div>
     </v-flex>
@@ -52,7 +51,7 @@ export default {
 
 <style scoped>
 .error {
-  color: red
+  color: red;
 }
 
 </style>
