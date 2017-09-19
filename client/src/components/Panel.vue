e77ee66293fb4077537ab0cb3b2c98e473a80786
rahid <hidayat.85@gmail.com>
2017-09-19 07:21:01 +0700

    create Panel components to used as default panel component in other components that need it

---
+++ client/src/components/Panel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="white elevation-3">
+    <v-toolbar flat dense class="cyan" dark>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+    </v-toolbar>
+
+    <div class="pl-4 pr-4 pt-2 pb-2">
+      <slot>
+        No slot content defined
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'hello world'
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,9 +1,17 @@
 <template>
-  
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <Panel />
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 export default {
+  components: {
+    Panel
+  }
 }
 
 </script>
