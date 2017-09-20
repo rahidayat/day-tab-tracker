a5822c4f99b7405aa0c9c3bb22d9dc54489b0f98
rahid <hidayat.85@gmail.com>
2017-09-20 07:42:40 +0700

    create model Song, create get and post song in backend

---
+++ server/src/controllers/SongsController.js
@@ -0,0 +1,29 @@
+const {Song} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+
+
+module.exports  = {
+  async index (req, res) {
+    try {
+      const songs = await Song.findAll({
+        limit: 10
+      })
+      res.send(songs)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to fetch the songs'
+      })
+    }
+  },
+  async post (req, res) {
+    try {
+      const song = await Song.create(req.body)
+      res.send(song)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to create the song'
+      })
+    }
+  }
+}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,6 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
 
 module.exports = (app) => {
   // app.get('/status', (req, res) => {
@@ -10,4 +11,7 @@ module.exports = (app) => {
 
   app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
   app.post('/login', AuthenticationController.login)
+
+  app.get('/songs', SongsController.index)
+  app.post('/songs', SongsController.post)
 }
